40dc0c74306b8b9b7e4f455d080144030ebb0127
cherukurim <myeleswa@Hotmail.com>
2025-05-12 16:06:23 +0000

    Second commit datatypes

---
+++ common-types/StringDemo.java
@@ -0,0 +1,13 @@
+public class StringDemo {
+    public static void main (String[] args) {
+     String greeting = "Hello WORLD!";
+     String name = "Alice";
+        String combined = greeting + " " + name;
+
+        System.out.println(greeting);
+        System.out.println(name);
+        System.out.println(combined);
+
+    }
+
+}

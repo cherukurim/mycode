2be84ab93bf8c1b4032775b61daebbc7375b39af
cherukurim <myeleswa@Hotmail.com>
2025-05-12 19:21:09 +0000

    java studies

---
+++ methods-lab/NoParameterMethod.java
@@ -0,0 +1,10 @@
+public class NoParameterMethod {
+    public static void main(String[] args) {
+        printMessage();
+    }
+
+    // A method that takes no parameters
+    public static void printMessage() {
+        System.out.println("Hello, this is a method with no parameters.");
+    }
+}
